be332ac903a12eca02cf9954f4b1aaeed287afd2
Sama Hosseni <sama@gatech.edu>
2015-01-19 11:37:55 -0500

    Sama modified file Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -31,19 +31,31 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+      String output = "";
+      for (int i = 2; i < input.length(); i++) {
+          output += input.charAt(i);
+      }
+      output += input.charAt(0);
+      output += input.charAt(1);
+	  return output;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
 
+    public static void main(String[] args) {
+        Person1 p1 = new Person1("Somayeh Hosseini Porgham");
+        System.out.println(p1.name);
+
+        p1.calc("gtshp7");
+    }
 }
